3279f5b822bae50d73d80a41a53930626bf3a25a
yiannnn <you@example.com>
2021-11-11 16:33:37 +0800

    first

---
+++ LineNotify/Controllers/ExaminersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LineNotify.Controllers
+{
+    public class ExaminersController : Controller
+    {
+        // GET: Examiners
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Finish()
+        {
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+            return View();
+        }
+    }
+}
